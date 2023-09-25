2d8e150d2bb7409411ff762b2cabc6eb3e0dc685
hemant2464 <114100847+hemant2464@users.noreply.github.com>
2023-09-25 15:46:31 +0530

    Time: 101 ms (63.34%), Space: 16.3 MB (42.2%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def isValidSudoku(self, board):
         seen = set()
-
+        
         for i in range(9):
             for j in range(9):
                 element = board[i][j]
                 if element != '.':
-                    # Check for duplicates in the same row, column, or subgrid
-                    if (element, i) in seen or (j, element) in seen or (i // 3, j // 3, element) in seen:
+                    if (element, i) in seen or (element, ~j) in seen or (element, i // 3, j // 3) in seen:
                         return False
                     seen.add((element, i))
-                    seen.add((j, element))
-                    seen.add((i // 3, j // 3, element))
-
+                    seen.add((element, ~j))
+                    seen.add((element, i // 3, j // 3))
+        
         return True
